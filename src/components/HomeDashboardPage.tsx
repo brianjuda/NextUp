4320c56433f4efc941eb86d9a24f0d472d1da76f
brianjuda <brian.juda@gmail.com>
2025-08-01 08:07:01 -0700

    removed fetchTrending search

--- src/components/HomeDashboardPage.tsx
+++ src/components/HomeDashboardPage.tsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import { useMedia } from "../hooks/context";
-import { fetchTrending, fetchTrendingMovies, fetchTrendingTV } from "../actions/search";
+import { fetchTrendingMovies, fetchTrendingTV } from "../actions/search";
 import { normalizeMediaItem } from "../actions/normalize";
 import type { MediaItem } from "../types/types";
 import MovieList from "./MovieList";
-// import MiniMovieList from './MiniMovieList';
 
 export default function HomeDashboardPage () {
     const data = useMedia();
