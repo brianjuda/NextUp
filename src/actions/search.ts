77440a75dc034b2557376d0887fbda0219bc6253
brianjuda <brian.juda@gmail.com>
2025-08-01 08:03:09 -0700

    Added MovieDetailsPage

--- src/actions/search.ts
+++ src/actions/search.ts
@@ -14,15 +14,28 @@ export async function searchMedia(query: string) {
     return data.results.filter((item: any) => item.media_type === "movie" || item.media_type === "tv");
 }
 
-export async function fetchTrending() {
-    const res = await fetch(`${BASE_URL}/trending/all/week?language=en-US&api_key=${API_KEY}`);
+export async function fetchTrendingMovies() {
+    const res = await fetch(`${BASE_URL}/trending/movie/week?language=en-US&api_key=${API_KEY}`);
 
     if (!res.ok) {
-        throw new Error (`TMDB trending failed: ${res.statusText}`);
+        throw new Error(`TMDB trending movies failed: ${res.statusText}`);
     }
 
     const data = await res.json();
 
-    //filter out results that are not movies or TV
-    return data.results.filter((item: any) => item.media_type === "movie" || item.media_type === "tv");
+    //filter out results that are not movies
+    return data.results;
+}
+
+export async function fetchTrendingTV() {
+    const res = await fetch(`${BASE_URL}/trending/tv/week?language=en-US&api_key=${API_KEY}`);
+
+    if (!res.ok) {
+        throw new Error(`TMDB trending TV failed: ${res.statusText}`);
+    }
+
+    const data = await res.json();
+
+    //filter out results that are not TV
+    return data.results;
 }
